6e745c228b3d83ccec317075839f1cfd72b34e57
Joe Bruce <joe.bruce@autodesk.com>
2021-05-05 20:27:33 -0700

    Add missed return

--- src/DrawingServer.js
+++ src/DrawingServer.js
@@ -2,14 +2,16 @@ const baseUrl = "http://localhost:1337";
 
 export default class DrawingServer {
   async addPoints(points) {
-    await fetch(`${baseUrl}/points/add`, {
-      method: "POST",
-      body: JSON.stringify(points),
-      cache: "no-cache",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    return await (
+      await fetch(`${baseUrl}/points/add`, {
+        method: "POST",
+        body: JSON.stringify(points),
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    ).json();
   }
 
   async getPoints() {
